8788315211a30b3e81139fe56f0c80ce4cdd9681
vikapitoshka <kapitonenko.vick@ya.ru>
2018-02-09 18:37:37 +0300

    counting of points

--- js/script.js
+++ js/script.js
@@ -13,25 +13,26 @@ var app = new Vue({
       { option: [ '5', '2', '4', '8' ] },
       { option: ['Жаранова', 'Иванова', 'Соколова'] }
     ],
-    show: 0,
+    id: 0,
     checkedValue: [],
     correct: [],
-    incorrect: []
+    incorrect: [],
+    show: 'test'
   },
   methods: {
     next: function() {   
       for (let i = 0; i < this.checkedValue.length; i++) {   
-        if (this.checkedValue[i] === this.answers[this.show].answer) {
-          this.correct.push(this.show);
+        if (this.checkedValue[i] === this.answers[this.id].answer) {
+          this.correct.push(this.id);
         } else {
-          this.incorrect.push(this.show);
+          this.incorrect.push(this.id);
         }
       }
       this.checkedValue = [];
-      if (this.show < this.questions.length) {
-        this.show++;
+      if (this.id < this.questions.length) {
+        this.id++;
       } else {
-        this.show = 0;
+        this.show = 'result';
       }
     }
   }
