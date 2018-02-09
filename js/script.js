fc3802476e2ed422fd0c41dbd4e9b0fc2849d0f6
vikapitoshka <kapitonenko.vick@ya.ru>
2018-02-09 17:13:55 +0300

    :hatching_chick:

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    questions: [
+      { question: 'Сколько будет 2 * 2?'/*, correct: 'Хорошо', options: ['Нормально', 'Хорошо', 'Отлично']*/ },
+      { question: 'Какая фамилия у Насти?' }
+    ],
+    answers: [
+      { answer: '4' },
+      { answer: 'Жаранова' }
+    ],
+    options: [
+      { option: [ '5', '2', '4', '8' ] },
+      { option: ['Жаранова', 'Иванова', 'Соколова'] }
+    ],
+    show: 0
+  }
+});
+
+
+
+
